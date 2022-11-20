84b2c825046472b18ca852785b75d738b890f4ee
JeetCh9 <jeetkumar0898@gmail.com>
2022-11-20 17:06:02 +0530

    Changes in cors policy

--- Middleware/OriginSetter.js
+++ Middleware/OriginSetter.js
@@ -1,5 +1,8 @@
 exports.setOriginHeader = (req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://parlour-frontend.vercel.app"
+  );
   next();
 };
