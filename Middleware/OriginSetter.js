de02c1e8ac13fa1783e2cde89cefcfc13c4e46ef
JeetCh9 <jeetkumar0898@gmail.com>
2022-11-20 17:18:16 +0530

    CORS

--- Middleware/OriginSetter.js
+++ Middleware/OriginSetter.js
@@ -2,7 +2,8 @@ exports.setOriginHeader = (req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Origin",
-    "https://parlour-frontend.vercel.app"
+    // "https://parlour-frontend.vercel.app"
+    "*"
   );
   next();
 };
